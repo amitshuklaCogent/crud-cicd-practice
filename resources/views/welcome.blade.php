e79b92c232db2e94b9f52a3877793bc8f831e71d
amitshuklaCogent <amit.shukla@cogenteservices.com>
2026-06-16 16:31:06 +0530

    again revert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-   <h1>Welcome to Laravel</h1>
+    <h1>Welcome to Laravel</h1>
+    <p>This is the welcome page.</p>
+    <p>This is the welcome page.2</p>
+    <p>This is the welcome page.3</p>
+    <p>This is the welcome page.4</p>
+    <p>Test Composer</p>
 </body>
 </html>
